b3d9815a1fe8429e4f0bc23f81a510d824715238
pushkarydv <96358784+pushkarydv@users.noreply.github.com>
2022-02-02 11:38:52 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,13 @@ export default function Footer() {
   return (
     <>
       <div className="w-100 text-slate-400 text-center py-2 text-xl">
-        &lt;/&gt; &amp; Crafted with 💛 by Pushkar Yadav
+        &lt;/&gt; &amp; Crafted with 💛 by{" "}
+        <a
+          href="https://github.com/pushkarydv"
+          className="no-underline text-teal-300"
+        >
+          Pushkar Yadav
+        </a>
       </div>
     </>
   );
