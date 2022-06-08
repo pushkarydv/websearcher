dbc2a91b81c2298afd192e9e750b489313707973
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-06-08 11:22:55 +0530

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,13 @@ import { NavLink, Link } from "react-router-dom";
 import "../App.css";
 export default function Navbar() {
   return (
-    <nav className="px-5 py-3 text-2xl flex flex-row justify-between items-center relative backdrop-blur-md bg-neutral-800 bg-opacity-70 z-50 ">
+    <nav className="px-5 py-3 text-2xl flex flex-row justify-between items-center relative backdrop-blur-md bg-slate-900 bg-opacity-70 z-50 ">
       <div className="font-medium">
         <Link className="no-underline " to="/">
           Web Searcher
         </Link>
       </div>
-      <div className="flex flex-row justify-between text-zinc-300">
+      <div className=" transition flex flex-row justify-between text-zinc-300 hover:scale-90">
         {[
           ["Home", "/"],
           ["Create", "/create"],
@@ -17,9 +17,8 @@ export default function Navbar() {
             className={({ isActive }) => {
               return isActive
                 ? "hidden"
-                : "transition px-4 md:px-8 md:m-1 py-2 bg-teal-400 shadow text-slate-900 font-medium rounded-3xl hover:bg-teal-300";
+                : "transition px-4 md:px-8 md:m-1 py-2 bg-teal-400 shadow text-slate-900 font-medium rounded-3xl hover:bg-teal-300 ";
             }}
-            key={title}
             to={url}
           >
             {title}
