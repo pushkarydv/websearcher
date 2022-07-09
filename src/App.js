9168d58d46753edf560cb0f091a199a2cdca9e4a
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-07-09 14:09:58 +0530

    tested broadcast

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Broadcast from "./components/Broadcast";
 import Create from "./pages/Create";
 import Home from "./pages/Home";
 
 export default function App() {
   return (
     <>
+      <Broadcast />
       <BrowserRouter>
         <Routes>
           <Route path="/create" element={<Create />} />
---
+++ src/components/Broadcast.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+
+export default function Broadcast() {
+  // const [isVisible, setIsVisible] = useState(false);
+  // const [title, setTitle] = useState("");
+  // const [link, setLink] = useState("");
+  useEffect(() => {
+    axios.get("https://pushkaryadav.in/api/broadcast").then((res) => {
+      console.log(res);
+      // if (res.data.title) {
+      //   setTitle(res.data.title);
+      //   setLink(res.data.link);
+      //   setIsVisible(true);
+      //   setTimeout(() => {
+      //     setIsVisible(false);
+      //   }, 15000);
+      // }
+    });
+  }, []);
+  return (
+    <>
+      {/* {isVisible && (
+        <div className="h-8">
+          <div className="flex justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 absolute w-full h-8 top-0 left-0 text-white text-xs sm:text-sm md:text-base ease-in-out transition-all">
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              {title}
+            </a>
+          </div>
+        </div>
+      )} */}
+    </>
+  );
+}
